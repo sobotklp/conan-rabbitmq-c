f5ade13e29c6e78a695ca7a51716e77870b73faa
Lewis Sobotkiewicz <lsobotkiewicz@kixeye.com>
2018-02-01 16:43:08 -0800

    Adding Travis config

--- build.py
+++ build.py
@@ -2,6 +2,6 @@ from conan.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(username="sobotklp", apple_clang_versions=["8.1"], archs=["x86_64"], args="--build missing")
+    builder = ConanMultiPackager(username="sobotklp", archs=["x86_64"], args="--build missing")
     builder.add_common_builds(shared_option_name="rabbitmq-c:shared", pure_c=False)
     builder.run()
