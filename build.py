8e4290f23c26bfb16a41a2951605510c9d45153d
Lewis Sobotkiewicz <lsobotkiewicz@kixeye.com>
2018-02-01 21:18:22 -0800

    Remove static builds on Windows

--- build.py
+++ build.py
@@ -1,7 +1,18 @@
 from conan.packager import ConanMultiPackager
-
+import platform
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(username="sobotklp", archs=["x86_64"], args="--build missing")
     builder.add_common_builds(shared_option_name="rabbitmq-c:shared", pure_c=False)
+
+    # The rabbitmq-c library cannot be built as a static library on Win32.
+    if platform.system() == "Windows":
+        static_builds = []
+        for settings, options, env_vars, build_requires in builder.builds:
+            if not ("rabbitmq-c:shared" in options and options["rabbitmq-c:shared"]):
+                static_builds.append([settings, options, env_vars, build_requires])
+
+        builder.builds = static_builds
+
     builder.run()
+
